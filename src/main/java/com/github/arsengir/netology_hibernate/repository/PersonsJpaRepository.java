30963254f18c711ee8124883125107cadce0c7d0
Arsen Girinov <amateurlib@yandex.ru>
2021-09-24 21:17:43 +0300

    task query

--- src/main/java/com/github/arsengir/netology_hibernate/repository/PersonsJpaRepository.java
+++ src/main/java/com/github/arsengir/netology_hibernate/repository/PersonsJpaRepository.java
@@ -3,6 +3,8 @@ package com.github.arsengir.netology_hibernate.repository;
 import com.github.arsengir.netology_hibernate.repository.entity.Persons;
 import com.github.arsengir.netology_hibernate.repository.entity.PersonsId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +14,12 @@ import java.util.Optional;
 @Repository
 public interface PersonsJpaRepository extends JpaRepository<Persons, PersonsId> {
 
-    List<Persons> findByCity(String City);
+    @Query(value = "select p from Persons p where p.city = :city")
+    List<Persons> findByCity(@Param("city") String City);
 
-    List<Persons> findByPersonsId_AgeLessThanOrderByPersonsIdAge(int PersonsIdAge);
+    @Query(value = "select p from Persons p where p.personsId.age < :age order by p.personsId.age")
+    List<Persons> findByPersonsId_AgeLessThanOrderByPersonsIdAge(@Param("age") int PersonsIdAge);
 
+    @Query(value = "select * from netology.persons p where p.name = ? and p.surname = ? limit 1", nativeQuery = true)
     Optional<Persons> findFirstByPersonsId_NameAndPersonsId_Surname(String name, String surname);
 }
