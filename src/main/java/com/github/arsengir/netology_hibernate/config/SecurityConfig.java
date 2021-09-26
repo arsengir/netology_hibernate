083557600c2defff919ebcb7173ca5213c00192a
Arsen Girinov <amateurlib@yandex.ru>
2021-09-26 18:58:34 +0300

    task spring_method_security

--- src/main/java/com/github/arsengir/netology_hibernate/config/SecurityConfig.java
+++ src/main/java/com/github/arsengir/netology_hibernate/config/SecurityConfig.java
@@ -2,22 +2,25 @@ package com.github.arsengir.netology_hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("Vlad").password("{noop}vlad").roles("READ");
+        auth.inMemoryAuthentication().withUser("Pavel").password("{noop}pavel").roles("WRITE", "READ");
+        auth.inMemoryAuthentication().withUser("Sergey").password("{noop}sergey").roles("DELETE", "READ");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons").permitAll()
-                .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
 }
--- src/main/java/com/github/arsengir/netology_hibernate/controller/PersonsController.java
+++ src/main/java/com/github/arsengir/netology_hibernate/controller/PersonsController.java
@@ -2,10 +2,14 @@ package com.github.arsengir.netology_hibernate.controller;
 
 import com.github.arsengir.netology_hibernate.repository.entity.Persons;
 import com.github.arsengir.netology_hibernate.service.PersonsService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,21 +23,25 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Persons> getPersonsByCity(@RequestParam("city") String city){
         return personsService.getPersonsByCity(city);
     }
 
     @GetMapping("/persons/less-age")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public List<Persons> getPersonsLessAge(@RequestParam("age") int age){
         return personsService.getPersonsLessAge(age);
     }
 
     @GetMapping("/persons")
+    @PostAuthorize("returnObject.get().personsId.name == authentication.principal.username")
     public Optional<Persons> getPersonsNameSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return personsService.getPersonsNameSurname(name, surname);
     }
 
     @GetMapping("/persons/init")
+    @RolesAllowed("ROLE_WRITE")
     public String initPersons() {
         return personsService.initPersons();
     }
